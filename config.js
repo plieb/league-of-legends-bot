6c6bccfc33b8fea4eb92cd44eb53ddac6f9f74fd
PE Lieb <lieb.pe@gmail.com>
2017-12-18 21:01:53 +0100

    [RM] unused folders and unused variables in config

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  // You can add your own open weather map token here (https://home.openweathermap.org/api_keys)
-  OPENWEATHER_TOKEN: process.env.OPENWEATHER_TOKEN || '',
-  // You can add your own tmdb token here (https://www.themoviedb.org/)
-  MOVIEDB_TOKEN: process.env.MOVIEDB_TOKEN || '',
-  // You can add your own tmdb token here (https://www.themoviedb.org/)
+  // You can get your own Panda Score token here (https://pandascore.co/)
   PANDA_TOKEN: process.env.PANDA_TOKEN || '',
   // Port
   PORT: process.env.PORT || 5000,
